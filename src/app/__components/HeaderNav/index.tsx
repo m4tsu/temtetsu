c7ee3d101caf2195115cb39699581eaf4034df80
m4tsu <matsurec0147@gmail.com>
2023-06-22 02:27:45 +0900

    add patch info

--- src/app/__components/HeaderNav/index.tsx
+++ src/app/__components/HeaderNav/index.tsx
@@ -5,13 +5,16 @@ import type { FC } from 'react'
 export const HeaderNav: FC = () => {
   return (
     <header className="border-b border-base-content">
-      <nav className="container max-w-[1024px] py-2  md:mx-auto">
+      <nav className="container max-w-[1024px] py-2  md:mx-auto flex justify-between items-center">
         <Link
           href="/"
-          className="btn-ghost btn text-3xl normal-case text-primary"
+          className="btn-ghost btn-primary btn text-3xl text-primary"
         >
           テム徹（仮）
         </Link>
+        <a href='https://crema.gg/temtem/patch-1-3/' target='_blank' rel='noreferrer' className='btn btn-outline btn-sm btn-primary' >
+         PATCH 1.3
+        </a>
       </nav>
     </header>
   )
