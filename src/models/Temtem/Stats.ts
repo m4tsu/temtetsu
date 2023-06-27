ed356313c56028fad3b24998f7803b30ef19d412
m4tsu <matsurec0147@gmail.com>
2023-06-28 04:02:26 +0900

    feat: add calculations

--- src/models/Temtem/Stats.ts
+++ src/models/Temtem/Stats.ts
@@ -1,3 +1,5 @@
+import { Species, Stats } from "./Species";
+
 const LEVEL = 100
 export const calculateHP = (
   base: number,
@@ -51,137 +53,135 @@ export const calculateTotalDurabilityIndex = ({
   return (hp * def * spdef) / (def + spdef)
 }
 
-// /** TVが0の時のstatsよりstatsが大きくなる最小のTVの値(HP) */
-// const getHpInterceptPointTv = (species: Species) => {
-//   const base = species.stats.hp
-//   const initial = calculateHP(base, { tv: 0 })
-//   for (let tv = 1; tv <= MAX_TV; tv++) {
-//     const stats = calculateHP(base, { tv })
-//     if (stats > initial) {
-//       return tv
-//     }
-//   }
-//   throw new Error('not found')
-// }
+/** TVが0の時のstatsよりstatsが大きくなる最小のTVの値(HP) */
+const getHpInterceptPointTv = (baseHp: number) => {
+  const initial = calculateHP(baseHp, { tv: 0 })
+  for (let tv = 1; tv <= MAX_TV; tv++) {
+    const stats = calculateHP(baseHp, { tv })
+    if (stats > initial) {
+      return tv
+    }
+  }
+  throw new Error('not found')
+}
 
-// /** TVが0の時のstatsよりstatsが大きくなる最小のTVの値(ATK, DEF, SPATK, SPDEF) */
-// const getOthersInterceptPointTv = (
-//   species: Species,
-//   statusName: OthersStatusName
-// ) => {
-//   const initial = calculateOthers(species, statusName, { tv: 0 })
-//   for (let tv = 1; tv <= MAX_TV; tv++) {
-//     const stats = calculateOthers(species, statusName, { tv })
-//     if (stats > initial) {
-//       return tv
-//     }
-//   }
-//   throw new Error('not found')
-// }
+/** TVが0の時のstatsよりstatsが大きくなる最小のTVの値(ATK, DEF, SPATK, SPDEF) */
+const getOthersInterceptPointTv = (
+  baseStats: number,
+) => {
+  const initial = calculateOthers(baseStats, { tv: 0 })
+  for (let tv = 1; tv <= MAX_TV; tv++) {
+    const stats = calculateOthers(baseStats, { tv })
+    if (stats > initial) {
+      return tv
+    }
+  }
+  throw new Error('not found')
+}
 
-// /** 割り振ることができるTVの合計の最大値 */
-// export const MAX_TOTAL_TV = 1000
-// /** 各ステータスに割り振ることができるTVの最大値 */
-// export const MAX_TV = 500
+/** 割り振ることができるTVの合計の最大値 */
+export const MAX_TOTAL_TV = 1000
+/** 各ステータスに割り振ることができるTVの最大値 */
+export const MAX_TV = 500
 
-// const HP_INCREMENT_INTERVAL = 4
-// const DEF_INCREMENT_INTERVAL = 5
-// const SPDEF_INCREMENT_INTERVAL = 5
+const HP_INCREMENT_INTERVAL = 4
+const DEF_INCREMENT_INTERVAL = 5
+const SPDEF_INCREMENT_INTERVAL = 5
 
-// /**
-//  * 総合耐久指数が最も高くなるような TV 配分を計算する
-//  */
-// export const calculateMostDurableTv = (species: Species, tvSum: number) => {
-//   type Result = {
-//     tvDistribution: {
-//       hp: number
-//       def: number
-//       spdef: number
-//       /** 余り */
-//       remainder: number
-//     }
-//     durabilityIndex: number
-//   }
+/**
+ * 総合耐久指数が最も高くなるような TV 配分を計算する
+ */
+export const calculateMostDurableTv = (stats: Stats, tvSum: number) => {
+  type Result = {
+    tvDistribution: {
+      hp: number
+      def: number
+      spdef: number
+      /** 余り */
+      remainder: number
+    }
+    durabilityIndex: number
+  }
 
-//   let result: Result = {
-//     tvDistribution: {
-//       hp: 0,
-//       def: 0,
-//       spdef: 0,
-//       remainder: 0,
-//     },
-//     durabilityIndex: calculateTotalDurabilityIndex({
-//       hp: calculateHP(species, { tv: 0 }),
-//       def: calculateOthers(species, 'def', { tv: 0 }),
-//       spdef: calculateOthers(species, 'spdef', { tv: 0 }),
-//     }),
-//   }
+  let result: Result = {
+    tvDistribution: {
+      hp: 0,
+      def: 0,
+      spdef: 0,
+      remainder: 0,
+    },
+    durabilityIndex: calculateTotalDurabilityIndex({
+      hp: calculateHP(stats.hp, { tv: 0 }),
+      def: calculateOthers(stats.def,  { tv: 0 }),
+      spdef: calculateOthers(stats.spdef,  { tv: 0 }),
+    }),
+  }
 
-//   const hpInterceptPointTv = getHpInterceptPointTv(species)
-//   const defInterceptPointTv = getOthersInterceptPointTv(species, 'def')
-//   const spdefInterceptPointTv = getOthersInterceptPointTv(species, 'spdef')
+  const hpInterceptPointTv = getHpInterceptPointTv(stats.hp)
+  const defInterceptPointTv = getOthersInterceptPointTv(stats.def)
+  const spdefInterceptPointTv = getOthersInterceptPointTv(stats.spdef)
 
-//   const maxTvOfEachStats = Math.min(tvSum, MAX_TV)
+  const maxTvOfEachStats = Math.min(tvSum, MAX_TV)
 
-//   for (
-//     let i = 0;
-//     (i === 0 ? 0 : hpInterceptPointTv + HP_INCREMENT_INTERVAL * (i - 1)) <=
-//     maxTvOfEachStats;
-//     i++
-//   ) {
-//     const hp =
-//       i === 0 ? 0 : hpInterceptPointTv + HP_INCREMENT_INTERVAL * (i - 1)
-//     for (
-//       let j = 0;
-//       (j === 0 ? 0 : defInterceptPointTv + DEF_INCREMENT_INTERVAL * (j - 1)) <=
-//       Math.min(MAX_TV, tvSum - hp);
-//       j++
-//     ) {
-//       const def =
-//         j === 0 ? 0 : defInterceptPointTv + DEF_INCREMENT_INTERVAL * (j - 1)
-//       const spdef = Math.min(MAX_TV, tvSum - hp - def)
+  for (
+    let i = 0;
+    (i === 0 ? 0 : hpInterceptPointTv + HP_INCREMENT_INTERVAL * (i - 1)) <=
+    maxTvOfEachStats;
+    i++
+  ) {
+    const hp =
+      i === 0 ? 0 : hpInterceptPointTv + HP_INCREMENT_INTERVAL * (i - 1)
+    for (
+      let j = 0;
+      (j === 0 ? 0 : defInterceptPointTv + DEF_INCREMENT_INTERVAL * (j - 1)) <=
+      Math.min(MAX_TV, tvSum - hp);
+      j++
+    ) {
+      const def =
+        j === 0 ? 0 : defInterceptPointTv + DEF_INCREMENT_INTERVAL * (j - 1)
+      const spdef = Math.min(MAX_TV, tvSum - hp - def)
 
-//       const statsHp = calculateHP(species, { tv: hp })
-//       const statsDef = calculateOthers(species, 'def', { tv: def })
-//       const statsSpdef = calculateOthers(species, 'spdef', {
-//         tv: spdef,
-//       })
-//       const durabilityIndex = calculateTotalDurabilityIndex({
-//         hp: statsHp,
-//         def: statsDef,
-//         spdef: statsSpdef,
-//       })
+      const statsHp = calculateHP(stats.hp, { tv: hp })
+      const statsDef = calculateOthers(stats.def, { tv: def })
+      const statsSpdef = calculateOthers(stats.spdef, {
+        tv: spdef,
+      })
+      const durabilityIndex = calculateTotalDurabilityIndex({
+        hp: statsHp,
+        def: statsDef,
+        spdef: statsSpdef,
+      })
 
-//       if (durabilityIndex > result.durabilityIndex) {
-//         result = {
-//           tvDistribution: {
-//             hp,
-//             def,
-//             spdef,
-//             remainder: 0,
-//           },
-//           durabilityIndex,
-//         }
-//       }
-//     }
-//   }
+      if (durabilityIndex > result.durabilityIndex) {
+        result = {
+          tvDistribution: {
+            hp,
+            def,
+            spdef,
+            remainder: 0,
+          },
+          durabilityIndex,
+        }
+      }
+    }
+  }
 
-//   // spdef は hp, def を余らないように振った後全て振るので余剰がある可能性がある。その場合はそれが分かるようにする
-//   const spdefRemainder =
-//     result.tvDistribution.spdef >= spdefInterceptPointTv
-//       ? (result.tvDistribution.spdef - spdefInterceptPointTv) %
-//         SPDEF_INCREMENT_INTERVAL
-//       : result.tvDistribution.spdef
-//   if (spdefRemainder !== 0) {
-//     result = {
-//       ...result,
-//       tvDistribution: {
-//         ...result.tvDistribution,
-//         spdef: result.tvDistribution.spdef - spdefRemainder,
-//         remainder: spdefRemainder,
-//       },
-//     }
-//   }
+  // spdef は hp, def を余らないように振った後全て振るので余剰がある可能性がある。その場合はそれが分かるようにする
+  const spdefRemainder =
+    result.tvDistribution.spdef >= spdefInterceptPointTv
+      ? (result.tvDistribution.spdef - spdefInterceptPointTv) %
+        SPDEF_INCREMENT_INTERVAL
+      : result.tvDistribution.spdef
+  if (spdefRemainder !== 0) {
+    result = {
+      ...result,
+      tvDistribution: {
+        ...result.tvDistribution,
+        spdef: result.tvDistribution.spdef - spdefRemainder,
+        remainder: spdefRemainder,
+      },
+    }
+  }
 
-//   return result
-// }
+  return result
+}
